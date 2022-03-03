750326b6ba409c22fe3c9bcc335bb7e0d56851c3
Silvan Wenk <silvan.wenk@stud.hslu.ch>
2022-03-03 17:47:45 +0100

    Add Entity Framework Core 6 project

---
+++ PushAlertsApi.Persistency/PushAlertsDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PushAlertsApi.Persistency
+{
+    public class PushAlertsDbContext : DbContext
+    {
+        public PushAlertsDbContext(DbContextOptions<PushAlertsDbContext> options) : base(options)
+        {
+            // TODO: Verify if constructor is needed.
+        }
+    }
+}
--- PushAlertsApi/Program.cs
+++ PushAlertsApi/Program.cs
@@ -1,10 +1,17 @@
+using System.Runtime.CompilerServices;
+using Microsoft.EntityFrameworkCore;
+using PushAlertsApi.Persistency;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddDbContext<PushAlertsDbContext>(options => 
+    options.UseMySQL(builder.Configuration.GetConnectionString("PushAlertsDb")));
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
