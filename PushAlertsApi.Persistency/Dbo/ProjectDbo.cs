b4a628ad5e893b86b21ac78b0a8087f645c68deb
Silvan Wenk <silvan.wenk@stud.hslu.ch>
2022-03-09 11:58:11 +0100

    Define database objects for ORM mapping

---
+++ PushAlertsApi.Persistency/Dbo/ProjectDbo.cs
@@ -0,0 +1,10 @@
+﻿namespace PushAlertsApi.Persistency.Dto
+{
+    public class ProjectDbo
+    {
+        public int Id { get; set; }
+        public Guid Uuid { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
--- PushAlertsApi.Persistency/PushAlertsDbContext.cs
+++ PushAlertsApi.Persistency/PushAlertsDbContext.cs
@@ -1,12 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using PushAlertsApi.Models;
+using PushAlertsApi.Persistency.Dto;
 
 namespace PushAlertsApi.Persistency
 {
     public class PushAlertsDbContext : DbContext
     {
-        public DbSet<Project> Projects { get; set; }
-        public DbSet<Models.Task> Tasks { get; set; }
-        public DbSet<User> Users { get; set; }
+        public DbSet<ProjectDbo> Projects { get; set; }
+        public DbSet<TaskDbo> Tasks { get; set; }
+        public DbSet<UserDbo> Users { get; set; }
     }
 }
