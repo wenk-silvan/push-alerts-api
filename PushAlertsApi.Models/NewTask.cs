7835bdb47748fb379bc407c56a029123d4c218e0
Silvan Wenk <silvan.wenk@stud.hslu.ch>
2022-04-20 10:48:05 +0200

    Remove unused imports

--- PushAlertsApi.Models/NewTask.cs
+++ PushAlertsApi.Models/NewTask.cs
@@ -1,7 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-using System.Text.Json.Serialization;
-
-namespace PushAlertsApi.Models
+﻿namespace PushAlertsApi.Models
 {
     public class NewTask
     {
