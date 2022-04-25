5b2f478bbf624e7274abae7261ace62aabea1e1b
Silvan Wenk <silvan.wenk@stud.hslu.ch>
2022-04-25 08:12:23 +0200

    Configure startup for jwt auth

--- PushAlertsApi/Program.cs
+++ PushAlertsApi/Program.cs
@@ -1,6 +1,9 @@
+using System.Text;
 using FirebaseAdmin;
 using Google.Apis.Auth.OAuth2;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using DataContext = PushAlertsApi.Data.DataContext;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +18,15 @@ builder.Services.AddDbContext<DataContext>(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetSection("Jwt:Key").Value)),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    });
 
 FirebaseApp.Create(new AppOptions()
 {
@@ -31,6 +43,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI(options => { options.SwaggerEndpoint("/swagger/v1/swagger.json", "V0.1"); });
 }
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
