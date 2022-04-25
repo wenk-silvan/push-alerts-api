8f094f5aa6bbf428b0c60a2c8cd3a7b84f4fef3a
Silvan Wenk <silvan.wenk@stud.hslu.ch>
2022-04-25 09:45:06 +0200

    Create new jwt upon login

--- PushAlertsApi/Controllers/AuthController.cs
+++ PushAlertsApi/Controllers/AuthController.cs
@@ -1,11 +1,21 @@
-﻿using System.Security.Cryptography;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using PushAlertsApi.Models;
 
 namespace PushAlertsApi.Controllers
 {
     public class AuthController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public static User user = new User();
 
         [HttpPost("register")]
@@ -28,7 +38,9 @@ namespace PushAlertsApi.Controllers
             {
                 return BadRequest("Wrong password.");
             }
-            return Ok("My token");
+
+            var token = CreateToken(user);
+            return Ok(token);
         }
 
         private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
@@ -36,6 +48,23 @@ namespace PushAlertsApi.Controllers
             using var hmac = new HMACSHA512();
             passwordSalt = hmac.Key;
             passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        }
+
+        private string CreateToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new(ClaimTypes.Email, user.Email)
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration.GetSection("Jwt:Key").Value));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: credentials);
+                var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+            return jwt;
         }
 
         private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
